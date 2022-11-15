f1fde395c0e75bdfdf97d35f7de04074f6405955
CillaBe <priscilla.behen@gmail.com>
2022-11-14 21:04:26 -0600

    Save Term button working to add and modify terms, now need to get courses to show

--- app/src/main/java/com/example/wguschedulingapp/UI/ViewTerm.java
+++ app/src/main/java/com/example/wguschedulingapp/UI/ViewTerm.java
@@ -6,9 +6,11 @@ import static com.example.wguschedulingapp.R.id.fade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.wguschedulingapp.Database.Repository;
+import com.example.wguschedulingapp.Entity.Term;
 import com.example.wguschedulingapp.R;
 
 public class ViewTerm extends AppCompatActivity {
@@ -20,12 +22,14 @@ public class ViewTerm extends AppCompatActivity {
     String name;
     String start;
     String end;
+    int termID;
     Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_term);
+        repository = new Repository(getApplication());
 
         TermStart = findViewById(R.id.editTermStart);
         TermEnd =findViewById(R.id.editTermEnd);
@@ -34,6 +38,7 @@ public class ViewTerm extends AppCompatActivity {
         name = getIntent().getStringExtra("name");
         start = getIntent().getStringExtra("start");
         end = getIntent().getStringExtra("end");
+        termID = getIntent().getIntExtra("id",-1);
 
         TermName.setText(name);
         TermStart.setText(start);
@@ -43,4 +48,15 @@ public class ViewTerm extends AppCompatActivity {
 
 
     }
-}
+    public void saveTerm(View view){
+        Term term;
+        if (termID == -1) {
+            int newID = repository.getAllTerms().get(repository.getAllTerms().size() - 1).getTermID() + 1;
+            term = new Term(newID, TermName.getText().toString(),TermStart.getText().toString(),TermEnd.getText().toString());
+            repository.insert(term);
+        } else {
+            term = new Term(termID, TermName.getText().toString(),TermStart.getText().toString(),TermEnd.getText().toString());
+            repository.update(term);
+        }
+    }
+    }
